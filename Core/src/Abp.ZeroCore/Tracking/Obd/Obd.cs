b24dcf740b6c3461146cd005c45a8cb17fddf35a
Sandeep Kumar <sandeeptripathi01@gmail.com>
2018-07-29 20:29:40 +0530

    Added the Obd table via migrations

---
+++ Core/src/Abp.ZeroCore/Tracking/Obd/Obd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Abp.Domain.Entities.Auditing;
+
+namespace Abp.Authorization.Users
+{
+    /// <summary>
+    /// Represents a user.
+    /// </summary>
+    public abstract class Obd<TObd> : ObdBase, IFullAudited<TObd>
+        where TObd : Obd<TObd>
+    {
+        /// <summary>
+        /// Maximum length of the <see cref="ConcurrencyStamp"/> property.
+        /// </summary>
+        public const int MaxConcurrencyStampLength = 128;
+        
+        /// <summary>
+        /// A random value that must change whenever a user is persisted to the store
+        /// </summary>
+        [StringLength(MaxConcurrencyStampLength)]
+        public virtual string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+
+        public virtual ICollection<UserToken> Tokens { get; set; }
+
+        public virtual TObd DeleterUser { get; set; }
+
+        public virtual TObd CreatorUser { get; set; }
+
+        public virtual TObd LastModifierUser { get; set; }
+
+        protected Obd()
+        {
+        }        
+    }
+}
